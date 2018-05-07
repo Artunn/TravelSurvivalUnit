7f6d89806c186fce66d557040bdaab022cfbb061
Yusuf Avci <yusufavci99@gmail.com>
2018-05-07 15:13:31 +0300

    Settings activity updated

--- app/src/main/java/com/example/asusnb/travelsurvivalunit/MainActivity.java
+++ app/src/main/java/com/example/asusnb/travelsurvivalunit/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.asusnb.travelsurvivalunit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void openSettings(View view) {
+        Intent intent = new Intent( this, SettingsActivity.class);
+        startActivity( intent);
     }
 }
