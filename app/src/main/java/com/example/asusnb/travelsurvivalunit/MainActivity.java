0a41ec28e9a78b8886cd043f97e1da5473ccdb84
Irmak <irmakdemir2203@gmail.com>
2018-05-09 17:12:38 +0300

    updateeeed

--- app/src/main/java/com/example/asusnb/travelsurvivalunit/MainActivity.java
+++ app/src/main/java/com/example/asusnb/travelsurvivalunit/MainActivity.java
@@ -5,8 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
