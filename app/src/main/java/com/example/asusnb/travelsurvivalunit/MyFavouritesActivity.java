ded84a82622b029050c7bc95db7868a6aff46ba8
yigitgorgulu <yigitgorgulu@yahoo.com>
2018-05-12 06:43:29 +0300

    my favourites design done.

---
+++ app/src/main/java/com/example/asusnb/travelsurvivalunit/MyFavouritesActivity.java
@@ -0,0 +1,13 @@
+package com.example.asusnb.travelsurvivalunit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyFavouritesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_favourites);
+    }
+}
