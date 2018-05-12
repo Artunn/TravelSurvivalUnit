e294430000038415125716471a9cd370a51e72ac
zegiTheWindy <39012491+zegiTheWindy@users.noreply.github.com>
2018-05-12 03:06:57 +0300

    City and Currency calculating classes

--- app/src/main/java/com/example/asusnb/travelsurvivalunit/Collections.java
+++ app/src/main/java/com/example/asusnb/travelsurvivalunit/Collections.java
@@ -1,23 +1,36 @@
 package com.example.asusnb.travelsurvivalunit;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Collections {
     //properties
-    HashMap<String, String> countries;
+    static HashMap<String, String> countries;
+    HashMap<String, String> cityTrad;
+    HashMap<String, String> cityFF;
+    ArrayList<String> traditions;
+    ArrayList<String> funFacts;
 
     //constructor
-    public Collections(){
+    public Collections() {
+        //Files need to be added
         countries = new HashMap<String, String>();
         countries.put("Turkey", "TRY");
         countries.put("England", "GBP");
+        traditions = new ArrayList<String>();
+        funFacts = new ArrayList<String>();
+        cityFF = new HashMap<String, String>();
     }
-
-    //methods
-    public void addCountry( String countryName, String countryCurr){
-        countries.put( countryName, countryCurr);
-    }
-    public String getCurrency( String country){
-        return countries.get( country);
-    }
+        //methods
+        public void addCountry(String countryName, String countryCurr){
+            countries.put(countryName, countryCurr);
+        }
+        public String getCurrency (String country){
+            return countries.get(country);
+        }
+//    private void setFF(){
+//        InputStream is = context.getAssets().open("subquestions");
+//        Scanner scan = new Scanner( is);
+//
+//    }
 }
