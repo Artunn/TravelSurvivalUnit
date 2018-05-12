a6659998c6321c29bc796b94c6913a622632d960
yigitgorgulu <yigitgorgulu@yahoo.com>
2018-05-12 10:21:31 +0300

    minor tweaks.

--- app/src/main/java/com/example/asusnb/travelsurvivalunit/MainActivity.java
+++ app/src/main/java/com/example/asusnb/travelsurvivalunit/MainActivity.java
@@ -11,13 +11,6 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        /*
-        City cityToUse;
-        User userToUse;
-
-        cityToUse =
-        */
     }
 
     public void openSettings(View view) {
