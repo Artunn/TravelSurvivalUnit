2683c84afa9dd5552acb4a58249c471ff243c434
Yusuf Avci <yusufavci99@gmail.com>
2018-05-12 07:25:29 +0300

    avcikacar

--- app/src/main/java/com/example/asusnb/travelsurvivalunit/SettingsActivity.java
+++ app/src/main/java/com/example/asusnb/travelsurvivalunit/SettingsActivity.java
@@ -6,13 +6,21 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity {
 
     User artun;
-
+    Button set;
+    EditText nameText;
+    EditText surnameText;
+    EditText languageText;
+    EditText destination;
+    EditText homeCountry;
+    EditText background;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +28,19 @@ public class SettingsActivity extends AppCompatActivity {
         artun = new User("artunn", "1234", "bursa", "artun",
                 "cura", "English", "Turkish", "Turkey",
                 null,"MAIL");
+
+
+        nameText = findViewById(R.id.nameEdit);
+        surnameText = findViewById(R.id.surnameEdit);
+        languageText = findViewById(R.id.languageEdit);
+        homeCountry = findViewById(R.id.homeCountryEdit);
+        background = findViewById(R.id.backgroundEdit);
+        set = findViewById(R.id.button4);
+        destination = findViewById(R.id.editText6);
+        nameText.setText(artun.getName());
+        surnameText.setText(artun.getSurname());
+        languageText.setText(artun.getMotherLanguage());
+        homeCountry.setText(artun.homeCountry);
     }
     public void optionTheme(View view) {
         String[] listItems = new String[] {"item1","item2"};
@@ -35,26 +56,30 @@ public class SettingsActivity extends AppCompatActivity {
         dialog.show();
     }
 
+    public void setUser(View view) {
+        artun.setName(nameText.getText().toString());
+        artun.setSurname(surnameText.getText().toString());
+        artun.setMotherLanguage(languageText.getText().toString());
+        artun.setHomeCountry(homeCountry.getText().toString());
+        artun.setDestination(destination.getText().toString());
+    }
+
     public void changeAvatarNext(View view) {
-        TextView text;
         ImageView imageView;
         int image;
 
-        text = findViewById( R.id.textView);
         imageView = findViewById( R.id.avatarImage);
         image = artun.nextAvatar();
-        text.setText( image);
         imageView.setImageResource( image);
+        artun.setAvatar( image);
     }
     public void changeAvatarPrevious(View view) {
-        TextView text;
         ImageView imageView;
         int image;
 
-        text = findViewById( R.id.textView);
         imageView = findViewById( R.id.avatarImage);
         image = artun.previousAvatar();
-        text.setText( image);
         imageView.setImageResource( image);
+        artun.setAvatar( image);
     }
 }
