16bfc75ebdbeabe9a3ea0be25b545eb66173eb27
Yusuf Avci <yusufavci99@gmail.com>
2018-05-13 06:18:57 +0300

    avcikacar

--- app/src/main/java/com/example/asusnb/travelsurvivalunit/MainActivity.java
+++ app/src/main/java/com/example/asusnb/travelsurvivalunit/MainActivity.java
@@ -37,4 +37,8 @@ public class MainActivity extends AppCompatActivity
         Intent intent = new Intent (this, MyFavouritesActivity.class);
         startActivity (intent);
     }
+    public void openWorldClocks (View view) {
+        Intent intent = new Intent (this, WorldClocks.class);
+        startActivity (intent);
+    }
 }
